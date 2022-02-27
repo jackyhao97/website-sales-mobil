23549bfd9b2210718a41731682dd5da78fc09640
Jacky <jackyhau97@gmail.com>
2022-02-27 15:44:55 +0700

    Update testimoni on home page.

--- index.php
+++ index.php
@@ -103,8 +103,21 @@
     </section>
     <section class="testimoni" id="testimoni">
       <div class="container-fluid bg-testimoni">
-        <img src="./assets/img/customer-testimonial.png" alt="Testimoni Mitsubishi Medan" class="customer-testimonial">
-        <img src="./assets/img/testimoni-1.png" alt="Testimoni Mitsubishi Medan">
+        <div class="row">
+          <div class="col-12">
+            <img src="./assets/img/customer-testimonial.png" alt="Testimoni Mitsubishi Medan" class="customer-testimonial">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col grid px-5">
+            <img src="./assets/img/testimoni-1.png" alt="Testimoni Mitsubishi Medan">
+            <img src="./assets/img/testimoni-1.png" alt="Testimoni Mitsubishi Medan">
+            <img src="./assets/img/testimoni-1.png" alt="Testimoni Mitsubishi Medan">
+            <img src="./assets/img/testimoni-1.png" alt="Testimoni Mitsubishi Medan">
+            <img src="./assets/img/testimoni-1.png" alt="Testimoni Mitsubishi Medan">
+            <img src="./assets/img/testimoni-1.png" alt="Testimoni Mitsubishi Medan">
+          </div>
+        </div>
       </div>
     </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
