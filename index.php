e1dec1dc9516a761994d3ba0b680e18338997f66
Jacky <jackyhau97@gmail.com>
2022-03-02 21:02:50 +0700

    Update navbar.

--- index.php
+++ index.php
@@ -31,34 +31,40 @@
     <title>Mitsubishi Medan</title>
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="background:black !important; border-bottom:3px solid red">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#"><img src="./assets/img/logo-jacky.png" style="width: 200px"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
+          <ul class="navbar-nav mmc-bold" style="font-size: 18px">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
+              <a class="nav-link active" aria-current="page" href="#">HOME</a>
             </li>
             <li class="nav-item dropdown">
               <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown link
+                PRODUK
               </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" style="background:black">
+                <li><a class="dropdown-item text-white nav-product" href="#"><img src="<?=BASE_URL.DS.'assets/img/thumbs/new-xpander-thumb.png'?>" style="width: 100px" class="me-4">NEW XPANDER</a></li>
+                <li><a class="dropdown-item text-white nav-product" href="#"><img src="<?=BASE_URL.DS.'assets/img/thumbs/xpander-cross-thumb.png'?>" style="width: 100px" class="me-4">NEW XPANDER CROSS</a></li>
+                <li><a class="dropdown-item text-white nav-product" href="#"><img src="<?=BASE_URL.DS.'assets/img/thumbs/pajero-sport-thumb.png'?>" style="width: 100px" class="me-4">PAJERO SPORT</a></li>
+                <li><a class="dropdown-item text-white nav-product" href="#"><img src="<?=BASE_URL.DS.'assets/img/thumbs/eclipse-cross-thumb.png'?>" style="width: 100px" class="me-4">ECLIPSE CROSS</a></li>
+                <li><a class="dropdown-item text-white nav-product" href="#"><img src="<?=BASE_URL.DS.'assets/img/thumbs/triton-thumb.png'?>" style="width: 100px" class="me-4">TRITON</a></li>
+                <li><a class="dropdown-item text-white nav-product" href="#"><img src="<?=BASE_URL.DS.'assets/img/thumbs/l300-thumb.png'?>" style="width: 100px" class="me-4">L300</a></li>
+                <li><a class="dropdown-item text-white nav-product" href="#"><img src="<?=BASE_URL.DS.'assets/img/thumbs/colt-diesel-thumb.png'?>" style="width: 100px" class="me-4">COLT DIESEL</a></li>
+                <li><a class="dropdown-item text-white nav-product" href="#"><img src="<?=BASE_URL.DS.'assets/img/thumbs/fuso-thumb.png'?>" style="width: 100px" class="me-4">FUSO</a></li>
               </ul>
             </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">TESTIMONI</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">PRICE LIST</a>
+            </li>
           </ul>
+          <a class="navbar-brand ms-auto d-none d-lg-block" href="#"><img src="./assets/img/logo-sardana-putih.png" style="width: 200px"></a>
         </div>
       </div>
     </nav>
