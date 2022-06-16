ed87e357e12d436490d35bcda08d98be84c5ca3f
Jacky <jackyhau97@gmail.com>
2022-06-16 20:50:59 +0700

    Add search and analytics console

--- config.php
+++ config.php
@@ -9,30 +9,30 @@
   $uri .= $_SERVER['HTTP_HOST'];  
   
   // local
-  define ('BASE_URL', $uri.'/website-sales-mobil');
+  // define ('BASE_URL', $uri.'/website-sales-mobil');
   // live
-  // define ('BASE_URL', $uri);
+  define ('BASE_URL', $uri);
   define('DS', '/');
   date_default_timezone_set('Asia/Jakarta');
 
   // Koneksi Live
-  // $servername = "localhost";
-  // $username = "mitq1622_hasri";
-  // $password = "Orig1nh4sri2021";
-  // $dbname = "mitq1622_hasri";
-  // $conn = mysqli_connect($servername, $username, $password, $dbname);
-  // if (!$conn) {
-  //   echo "Failed to connect!";
-  // }
-  // Akhir Koneksi Live
-
-  // Koneksi Local
   $servername = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "mitsubishi-vandys";
+  $username = "harb2847_eben";
+  $password = "Orig1neben2022";
+  $dbname = "harb2847_eben";
   $conn = mysqli_connect($servername, $username, $password, $dbname);
   if (!$conn) {
     echo "Failed to connect!";
   }
+  // Akhir Koneksi Live
+
+  // Koneksi Local
+  // $servername = "localhost";
+  // $username = "root";
+  // $password = "";
+  // $dbname = "mitsubishi-vandys";
+  // $conn = mysqli_connect($servername, $username, $password, $dbname);
+  // if (!$conn) {
+  //   echo "Failed to connect!";
+  // }
   // Akhir Koneksi Local
--- mitsubishi-fuso/index.php
+++ mitsubishi-fuso/index.php
@@ -19,6 +19,17 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;800&display=swap" rel="stylesheet"> 
 
+    <!-- Google Analytics -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y05V5NDS42"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-Y05V5NDS42');
+    </script>
+
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/00610b519d.js" crossorigin="anonymous"></script>
 
@@ -30,6 +41,26 @@
 
     <!-- Custom CSS -->
     <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?=BASE_URL.DS.'assets/img/'?>apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?=BASE_URL.DS.'assets/img/'?>apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?=BASE_URL.DS.'assets/img/'?>apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?=BASE_URL.DS.'assets/img/'?>apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="<?=BASE_URL.DS.'assets/img/'?>apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?=BASE_URL.DS.'assets/img/'?>apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?=BASE_URL.DS.'assets/img/'?>apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?=BASE_URL.DS.'assets/img/'?>apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="<?=BASE_URL.DS.'assets/img/'?>favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="<?=BASE_URL.DS.'assets/img/'?>favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="<?=BASE_URL.DS.'assets/img/'?>favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="<?=BASE_URL.DS.'assets/img/'?>favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="<?=BASE_URL.DS.'assets/img/'?>favicon-128.png" sizes="128x128" />
+    <meta name="application-name" content="&nbsp;"/>
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="msapplication-TileImage" content="mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="mstile-70x70.png" />
+    <meta name="msapplication-square150x150logo" content="mstile-150x150.png" />
+    <meta name="msapplication-wide310x150logo" content="mstile-310x150.png" />
+    <meta name="msapplication-square310x310logo" content="mstile-310x310.png" />
     <title>Mitsubishi Fuso Medan</title>
   </head>
   <body>
